fa5adaa8bfb334fb1d85cd3207ad7e4f1ae1daa7
Jacob Lewis <JacobMLewis@Live.com>
2025-11-30 00:37:21 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Pythonipelago
-A lightweight python library for intigration with the Archipelago platform.
+A lightweight python library for intigration with the Archipelago platform.  
+https://packaging.python.org/en/latest/tutorials/packaging-projects/  
+
+dependencies = [
+  "websockets",
+  "requests",
+  "json",
+  "uuid",
+  "typing",
+  "asyncio",
+  "time",
+]
+
+
+
+## Core
+Websocket frontend used to directly interact with the Archipelago Server  
+-- Send all information  
+-- Receive all information  
+-- Execute misc commands  
+
+## API
+Used to make API calls to the WebHost  
+-- All API calls  
+-- Whatever else to put here  
+
+## Util
+Common utilities to make working with Core+API easier.  
+-- Helper functions  
+-- Other useful utilitiy functions that don't fall within Core or API.  
---
+++ src/pythonipelago_quasky/core.py
@@ -0,0 +1,49 @@
+# ______            _                 _             _                   
+#(_____ \      _   | |               (_)           | |                  
+# _____) )   _| |_ | | _   ___  ____  _ ____   ____| | ____  ____  ___  
+#|  ____/ | | |  _)| || \ / _ \|  _ \| |  _ \ / _  ) |/ _  |/ _  |/ _ \ 
+#| |    | |_| | |__| | | | |_| | | | | | | | ( (/ /| ( ( | ( ( | | |_| |
+#|_|     \__  |\___)_| |_|\___/|_| |_|_| ||_/ \____)_|\_||_|\_|| |\___/ 
+#       (____/                         |_|                 (_____|      
+#  ______ _____  ______  _______                                        
+# / _____) ___ \(_____ \(_______)                                       
+#| /    | |   | |_____) )_____                                          
+#| |    | |   | (_____ (|  ___)                                         
+#| \____| |___| |     | | |_____                                        
+# \______)_____/      |_|_______)                                       
+
+import json
+import os
+import time
+import uuid
+import typing
+import websockets
+import asyncio
+
+
+
+async def Tracker():
+    print()
+    
+    
+    
+def start_client(server,port,slot,handling,keys):
+    print()
+    
+def stop_client(client):
+    print()
+    
+def get_client_status(client):
+    print()
+
+def check_queue(queue):
+    print()
+    
+def pop_queue(queue):
+    print()
+
+def push_queue(queue, item):
+    print()
+    
+
+
